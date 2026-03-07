1039e65a4bbdfcd58299ac4984bd5e329f1b904e
Lahiru <devindalahiru2004@gmail.com>
2026-03-07 10:12:32 +0530

    fix radio button problem.

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,32 @@
-export const cart =[];
+export const cart =[{productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2},
+  {productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1}
+];
+
+export function addToCart(productId){
+  let matchingItem;
+
+    cart.forEach((item) =>{
+            if(item.productId === productId){
+                matchingItem = item;
+            }    
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+}
+
+export function updateCartQuantity() {
+
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
