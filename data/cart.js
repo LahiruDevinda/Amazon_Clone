94153b6422f82dc813cbea9515cd46cfa939f833
Lahiru <devindalahiru2004@gmail.com>
2026-03-07 11:26:33 +0530

    create method for delete item from cart.

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart =[{productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2},
+export let cart =[{productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2},
   {productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1}
 ];
 
@@ -29,4 +29,14 @@ export function updateCartQuantity() {
   });
 
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+export function removeFromCart(productID) {
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productID) {
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
 }
