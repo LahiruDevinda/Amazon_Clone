b653cfa2a488681b07cd7c73d80465091ae43a64
Lahiru <devindalahiru2004@gmail.com>
2026-03-08 19:47:27 +0530

    Cart test created.

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,8 @@
 import { products } from "./products.js";
 
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-    cart = [];
-}
+loadFromStorage();
 
 export function addToCart(productId, quantity, deliveryOptionId) {
   let matchingItem;
@@ -73,4 +71,22 @@ export function calculateCartTotal() {
     });
   });
   return cartTotal;
+}
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      },{
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 2,
+        deliveryOptionId: '2'
+      }
+    ];   
+  }
 }
